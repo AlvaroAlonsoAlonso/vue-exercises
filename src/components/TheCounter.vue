8e822c9c413d17a3d7c2d306375a48d1ab751ab8
Alonso <alvaro.alonso@alebateducation.com>
2025-01-27 12:43:50 +0100

    exercise-6

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,29 +1,34 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import TheTitle from '@/components/TheTitle.vue'
-const count = ref(0)
+const count = ref(1)
 
-const isLessZero = computed(() => count.value <= 0)
+const NUMBER_ZERO = 0
+const NUMBER_TEN = 10
+
 const increment = () => {
   count.value++
 }
 
 const decrement = () => {
-  if (isLessZero.value) return
   count.value--
 }
+
+const isZero = computed(() => NUMBER_ZERO < count.value)
+const isTenOrMayor = computed(() => count.value < NUMBER_TEN)
 
 const countNumber = computed(() => count.value)
 </script>
 <template>
   <section class="counter">
     <TheTitle>
-      <h2>Exercise 5</h2>
+      <h2>Exercise 6</h2>
     </TheTitle>
     <button
       class="counter__button"
       aria-label="It's a button that you press to increment a number."
       @click="increment"
+      v-if="isTenOrMayor"
     >
       Increment
     </button>
@@ -31,6 +36,7 @@ const countNumber = computed(() => count.value)
       class="counter__button"
       aria-label="A button that decreases a number when pressed to zero."
       @click="decrement"
+      v-if="isZero"
     >
       Decrement
     </button>
