e3fd57c24bc42df532ab79fee95d739b916e9351
Alonso <alvaro.alonso@alebateducation.com>
2025-01-27 11:14:50 +0100

    exercise 4

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import TheTitle from '@/components/TheTitle.vue'
 </script>
 
 <template>
-  <TheTitle propTitle="Exercices 3" />
+  <TheTitle>
+    <h2>Exercise 4</h2>
+  </TheTitle>
   <RouterView />
 </template>
--- src/components/TheTitle.vue
+++ src/components/TheTitle.vue
@@ -1,10 +1,3 @@
-<script lang="ts" setup>
-import { defineProps } from 'vue'
-
-defineProps({
-  propTitle: String,
-})
-</script>
 <template>
-  <h2>{{ propTitle }}</h2>
+  <slot />
 </template>
